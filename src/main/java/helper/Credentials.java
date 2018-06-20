d67375b5327ed9f0a57583e6a1848dad1ff327f9
azmat428 <azmat428@gmail.com>
2018-06-19 20:03:54 -0400

    all the file

---
+++ src/main/java/helper/Credentials.java
@@ -0,0 +1,9 @@
+package helper;
+
+public class Credentials {
+
+	public static final String username = "invalidUN";
+	public static final String password = "invalidPW";
+	public static final String url = "http://newtours.demoaut.com/";
+	public static final String chromeDriverPath = "E:/selenium LB/chromedriver.exe";
+}
---
+++ src/test/resources/Feature/test.feature
@@ -0,0 +1,3 @@
+Feature: abc
+Scenario: for examole
+ 
